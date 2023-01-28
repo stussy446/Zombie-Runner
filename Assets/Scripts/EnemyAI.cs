e17702a6200a294ee743ed41075e18e00a8a20a6
Steve Rector <steverector27@gmail.com>
2023-01-28 11:17:17 -0800

    Add logic for enemy to enter attack mode
    enters mode if within stopping distance of the target

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,16 +20,54 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
+        CalculateDistanceToTarget();
+
         if (TargetInRange())
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
-        
     }
-    
-    private bool TargetInRange()
+
+    private void CalculateDistanceToTarget()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
+    }
+
+    private void EngageTarget()
+    {
+        if (TargetInAttackRange())
+        {
+            AttackTarget();
+        }
+        else
+        {
+            ChaseTarget();
+        }
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log("ATTACK");
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private bool TargetInRange()
+    {
         return distanceToTarget <= chaseRange;
+    }
+
+    private bool TargetInAttackRange()
+    {
+        return distanceToTarget <= navMeshAgent.stoppingDistance;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
