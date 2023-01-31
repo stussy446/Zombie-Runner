235d180c2086dfd472d217bb73cc18c3132f38f9
Steve Rector <steverector27@gmail.com>
2023-01-30 16:35:24 -0800

    Add raycast targeting that only recognizes enemies
    shoots raycast out from tip of gun and detects collisons on the enemy layer

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,9 +5,85 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    internal void Fire()
+    ShootInput weaponInputs;
+    Camera cam;
+    RaycastHit hit;
+    bool isShooting;
+    bool readyToShoot;
+
+    [SerializeField] float bulletRange;
+    [SerializeField] LayerMask shootableLayer;
+    [SerializeField] bool debugMode;
+    
+    private void Awake()
     {
-        Debug.Log($"{this.gameObject.name} fired");
+        weaponInputs = new ShootInput();
+        cam = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        weaponInputs.Enable();
+        weaponInputs.Shoot.Fire.started += ctx => Startshot();
+        weaponInputs.Shoot.Fire.canceled += ctx => EndShot();
+
+    }
+
+    private void Update()
+    {
+        if (isShooting)
+        {
+            PerformShot();
+        }
+    }
+
+    private void Startshot()
+    {
+        Debug.Log("Shot");
+        isShooting = true;
+    }
+
+    private void EndShot()
+    {
+        Debug.Log("Shot ended");
+        isShooting = false;
+    }
+
+    private void PerformShot()
+    {
+        readyToShoot = false;
+        Vector3 direction = cam.transform.forward;
+
+        if (debugMode)
+        {
+            Debug.DrawRay(cam.transform.position, direction * bulletRange, Color.red, 20f, false);
+        }
+
+
+        if (Physics.Raycast(cam.transform.position, direction, out hit, bulletRange, shootableLayer))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    private void ResetShot()
+    {
+
+    }
+
+    private void Reload()
+    {
+
+    }
+
+    private void ReloadFinish()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        weaponInputs.Disable();
     }
 
 }
--- Assets/Scripts/WeaponsManager.cs
+++ Assets/Scripts/WeaponsManager.cs
@@ -4,16 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerWeaponController : MonoBehaviour
+public class WeaponsManager : MonoBehaviour
 {
     [SerializeField] Weapon[] availableWeapons;
 
-    ShootInput shootInput;
     Weapon equippedWeapon;
 
     private void Awake()
     {
-        shootInput = new ShootInput();
         if (availableWeapons.Length < 2)
         {
             equippedWeapon = availableWeapons[0];
@@ -22,26 +20,6 @@ public class PlayerWeaponController : MonoBehaviour
         {
             equippedWeapon = availableWeapons[0];
             //TODO add ability to choose starting weapon
-
         }
-
-    }
-
-    private void OnEnable()
-    {
-        shootInput.Enable();
-
-        shootInput.Shoot.Fire.performed += OnShoot;
-    }
-
-    private void OnDisable()
-    {
-        shootInput.Shoot.Fire.performed -= OnShoot;
-        shootInput.Disable();
-    }
-
-    private void OnShoot(InputAction.CallbackContext obj)
-    {
-        equippedWeapon.Fire();
     }
 }
